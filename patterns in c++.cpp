074dee57db24192527f454623715dbbb29286ceb
rishi7822 <82447941+rishi7822@users.noreply.github.com>
2021-12-19 21:28:14 +0530

    Add files via upload

---
+++ patterns in c++.cpp
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   cout<<"\t \t  *"<<endl;
+   cout<<"\t + \t\t   -  ";
+   cout<<"\n  ( \t  #  \t)";
+   cout<<"\n \t % \t\t x";
+   cout<<"\n \t \t \ ";
+   return 0;
+}
